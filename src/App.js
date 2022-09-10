c30d35c826ad1ecb3c76391ab45f5e2f54b8a7a4
Jasmine <fancheny@cn.ibm.com>
2022-09-10 21:50:45 +0800

    lazy suspense

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
-import './App.css';
-import { createContext, Component } from 'react'
 
-const FirstContext = createContext()
+import React, { Component, lazy, Suspense } from 'react'
+const About = lazy(() => import('./About.js'))
+
 class App extends Component {
   state = {
-    first: 60
+    hasError: false
+  }
+  static getDerivedStateFromError(){
+    return {
+      hasError:true
+    }
   }
   render() {
-    const { first } = this.state
+    if(this.state.hasError) {
+      return <div>error</div>
+    }
     return (
-      <FirstContext.Provider value={first}>
-        <button
-          type="button"
-          onClick={() => { this.setState({ first: first - 1 }) }}> 按钮
-        </button>
-        <Middle />
-      </FirstContext.Provider>
+      <div>
+        <Suspense fallback={<div>loading</div>}>
+          <About />
+        </Suspense>
+      </div>
     )
   }
 }
-function Middle() {
-  return <Last />
-}
-class Last extends Component {
-  static contextType = FirstContext    //只有一个context 的时候
-  render() {
-    const FirstComponentValue = this.context
-    return (
-      <h1>这个地方是last 组件渲染的结果但是显示的是first 里面的值{FirstComponentValue}</h1>
-    )
-  }
-}
+
 export default App;
