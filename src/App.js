8725ceeb5bbc4378cb4a265376dbda4419e1160c
Jasmine <fancheny@cn.ibm.com>
2022-09-11 22:38:34 +0800

    clearInterval useRef

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
-import React, {Component, useState,useMemo,memo,useCallback,useRef,PureComponent } from 'react';
+import React, { useState, useRef, useCallback } from 'react'
 
+export default function App() {
+    const [now, setNow] = useState(Date.now())
+    const ref = useRef()
 
-class Counter extends Component{
-  speak(){
-    console.log('now Counter is '+this.props.count)
-  }
-  render(){
-    const {props} =this
-    return(
-      <h1 onClick={props.onClick}>{props.count}</h1>
+    const handleStart = useCallback(() => {
+        ref.current = setInterval(() => {
+            setNow(Date.now())
+        }, 1000)
+    }, [])
+
+     const handleStop = useCallback(() => {
+         clearInterval(ref.current)
+     }, [])
+
+    return (
+        <>
+            <h1>Now Time : {now}</h1>
+            <button onClick={handleStart}>Start</button>
+            <button onClick={handleStop}>Stop</button>
+        </>
     )
-  }
 }
-
-const App=()=>{
-  const [count,setCount]=useState(0)
-
-  const double=useMemo(()=>{
-    return count*2
-  },[count==3])
-
-  const onClick=useCallback(()=>{
-    console.log('onClick')
-    console.log(counterRef.current)
-    counterRef.current.speak()
-  },[])
-
-  const counterRef=useRef()
-
-  return ( 
-    <div> 
-      <button onClick={()=>setCount(count+1)}>click</button>     
-      <Counter ref={counterRef} count={double} onClick={onClick} />
-    </div>
-   );
-}
- 
-export default App;
