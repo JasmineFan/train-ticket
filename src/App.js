c53394c39c4b8b0868c54a4ffd35f73bbc187db3
Jasmine <fancheny@cn.ibm.com>
2022-09-11 21:35:19 +0800

    useMemo useCallback

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 
-import React, { useState, createContext, useContext} from 'react'
+import React, { useState, useMemo, memo, useCallback} from 'react'
 
-const CountContext = createContext()
-function Child(){
-  const count = useContext(CountContext)
-  return <div>{count}</div>
-}
+const Child = memo(function Child(props){
+  console.log('render child')
+  return <div onClick={props.onclick}>{props.count}</div>
+})
 function App(){
   const [count, setCount] = useState(0)
+
+  const double = useMemo(()=>{
+    return count*2
+  },[count===3])
+
+  const onclick = useCallback(()=>{
+    console.log('click')
+  },[])
+
+
+
   return (
     <div>
         <button 
           onClick={()=>{setCount(count+1)}}>
-            Click{count} 
+            Click{count} , {double}
           </button>
-          <CountContext.Provider value ={count}>
-            <Child/>
-          </CountContext.Provider>
+            <Child count={double} onClick={onclick}/>
       </div>
   )
 }
