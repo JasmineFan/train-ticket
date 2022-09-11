988271a0a523fbee99d54a2a6347dd32a6783dd3
Jasmine <fancheny@cn.ibm.com>
2022-09-11 22:13:14 +0800

    useRef1

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
+import React, {Component, useState,useMemo,memo,useCallback,useRef,PureComponent } from 'react';
 
-import React, { useState, useMemo, memo, useCallback} from 'react'
 
-const Child = memo(function Child(props){
-  console.log('render child')
-  return <div onClick={props.onclick}>{props.count}</div>
-})
-function App(){
-  const [count, setCount] = useState(0)
+class Counter extends Component{
+  speak(){
+    console.log('now Counter is '+this.props.count)
+  }
+  render(){
+    const {props} =this
+    return(
+      <h1 onClick={props.onClick}>{props.count}</h1>
+    )
+  }
+}
 
-  const double = useMemo(()=>{
+const App=()=>{
+  const [count,setCount]=useState(0)
+
+  const double=useMemo(()=>{
     return count*2
-  },[count===3])
+  },[count==3])
 
-  const onclick = useCallback(()=>{
-    console.log('click')
+  const onClick=useCallback(()=>{
+    console.log('onClick')
+    console.log(counterRef.current)
+    counterRef.current.speak()
   },[])
 
+  const counterRef=useRef()
 
-
-  return (
-    <div>
-        <button 
-          onClick={()=>{setCount(count+1)}}>
-            Click{count} , {double}
-          </button>
-            <Child count={double} onClick={onclick}/>
-      </div>
-  )
+  return ( 
+    <div> 
+      <button onClick={()=>setCount(count+1)}>click</button>     
+      <Counter ref={counterRef} count={double} onClick={onClick} />
+    </div>
+   );
 }
-export default App
+ 
+export default App;
