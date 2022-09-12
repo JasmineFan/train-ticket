c0cf127a53dc0cfd1a6b7363bf20af5a50bde314
Jasmine <fancheny@cn.ibm.com>
2022-09-12 20:26:11 +0800

    自定义hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React, { useState, useRef, useCallback } from 'react'
+import React, {Component, useState,useMemo,memo,useCallback,useRef,PureComponent, useEffect } from 'react';
 
-export default function App() {
-    const [now, setNow] = useState(Date.now())
-    const ref = useRef()
-
-    const handleStart = useCallback(() => {
-        ref.current = setInterval(() => {
-            setNow(Date.now())
-        }, 1000)
-    }, [])
-
-     const handleStop = useCallback(() => {
-         clearInterval(ref.current)
-     }, [])
-
-    return (
-        <>
-            <h1>Now Time : {now}</h1>
-            <button onClick={handleStart}>Start</button>
-            <button onClick={handleStop}>Stop</button>
-        </>
-    )
+function useCounter(count){
+  const size = useSize()
+  return (
+    <h1>{count}, {size.height} , {size.width}</h1>
+  )
 }
+function useCount(defaultCount){
+  const [count,setCount]=useState(defaultCount)
+  const it =useRef()
+  useEffect(()=>{
+    it.current = setInterval(()=>{
+      setCount(count=>count+1)
+    },1000)
+  },[])
+  useEffect(()=>{
+    if(count>=5){
+      clearInterval(it.current)
+    }
+  })
+  return [count,setCount]
+}
+function useSize(){
+  const [size,setSize] = useState({
+    width:document.documentElement.clientWidth,
+    height:document.documentElement.clientHeight
+  })
+  const onResize = useCallback(()=>{
+    setSize({
+      width:document.documentElement.clientWidth,
+      height:document.documentElement.clientHeight
+    })
+  },[])
+  useEffect(()=>{
+    window.addEventListener('resize',onResize, false)
+    return ()=>{
+      window.removeEventListener('resize',onResize, false)
+    }
+  },[])
+  return size
+}
+const App=()=>{
+  const [count,setCount]=useCount(0)
+  const Counter = useCounter(0)
+  const size = useSize()
+
+  return ( 
+    <div> 
+      <button onClick={()=>setCount(count+1)}>click {count}</button>     
+      {Counter} {size.height} , {size.width}
+    </div>
+   );
+}
+ 
+export default App;
