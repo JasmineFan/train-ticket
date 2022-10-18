f795d189db9152ee93ce3a7a6c20615558594d31
Jasmine <fancheny@cn.ibm.com>
2022-10-18 20:40:40 +0800

    没有redux 的时候写了一个todolist react 18

--- src/App.js
+++ src/App.js
@@ -1,56 +1,118 @@
-import React, {Component, useState,useMemo,memo,useCallback,useRef,PureComponent, useEffect } from 'react';
+import React, { Component, useState, useMemo, memo, useCallback, useRef, PureComponent, useEffect } from 'react';
+import './App.css'
 
-function useCounter(count){
-  const size = useSize()
+let idSeq = Date.now()
+
+function Control(props) {
+  const { addTodo } = props
+  const inputRef = useRef()
+  const onSubmit = (e) => {
+    e.preventDefault()      //阻止默认提交
+    const newText = inputRef.current.value.trim()
+    if (newText.length === 0) {
+      return;
+    }
+    addTodo({
+      id: ++idSeq,
+      text: newText,
+      complete: false
+    })
+    inputRef.current.value = ''
+  }
   return (
-    <h1>{count}, {size.height} , {size.width}</h1>
+    <div className='control'>
+      <h1>todos</h1>
+      <form onSubmit={onSubmit}>
+        <input ref={inputRef} type="text" className='new-todo' placeholder='what to do'></input>
+      </form>
+    </div>)
+}
+
+function TodoItem(props) {
+  const { todo: {
+    id, text, complete
+  }
+    , toggleTodo, removeToDo } = props
+  const onChange = () => {
+    toggleTodo(id)
+  }
+  const onRemove = () => { removeToDo(id)}
+  return (
+    <li className='todo-item'>
+      <input
+        type="checkbox"
+        onChange={onChange}
+        checked={complete}
+      />
+      <label className={complete ? 'complete' : ''}>{text}</label>
+      <button onClick={onRemove}>&#xd7;</button>
+
+    </li>
   )
 }
-function useCount(defaultCount){
-  const [count,setCount]=useState(defaultCount)
-  const it =useRef()
-  useEffect(()=>{
-    it.current = setInterval(()=>{
-      setCount(count=>count+1)
-    },1000)
-  },[])
-  useEffect(()=>{
-    if(count>=5){
-      clearInterval(it.current)
-    }
-  })
-  return [count,setCount]
-}
-function useSize(){
-  const [size,setSize] = useState({
-    width:document.documentElement.clientWidth,
-    height:document.documentElement.clientHeight
-  })
-  const onResize = useCallback(()=>{
-    setSize({
-      width:document.documentElement.clientWidth,
-      height:document.documentElement.clientHeight
-    })
-  },[])
-  useEffect(()=>{
-    window.addEventListener('resize',onResize, false)
-    return ()=>{
-      window.removeEventListener('resize',onResize, false)
-    }
-  },[])
-  return size
-}
-const App=()=>{
-  const [count,setCount]=useCount(0)
-  const Counter = useCounter(0)
-  const size = useSize()
+function Todos(props) {
+  const { todos, toggleTodo, removeToDo } = props
+  return (
+    <ul>
+      {
+        todos.map(todo => {
+          return <TodoItem
+            key={todo.id}
+            todo={todo}
+            toggleTodo={toggleTodo}
+            removeToDo={removeToDo}
 
-  return ( 
-    <div> 
-      <button onClick={()=>setCount(count+1)}>click {count}</button>     
-      {Counter} {size.height} , {size.width}
-    </div>
-   );
+          />
+        }
+
+        )
+      }
+    </ul>
+  )
 }
- 
-export default App;
+
+const LS_KEY = '_$todo'
+
+function TodoList() {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = useCallback((todo) => {
+    setTodos(todos => [...todos, todo])
+  }, [])
+
+  const removeToDo = useCallback((id) => {
+    setTodos(todos => todos.filter(todo => {
+      return todo.id != id
+    }))
+  }, [])
+
+  const toggleTodo = (id) => {
+    setTodos(todos => todos.map(todo => {
+      return todo.id === id
+        ? {
+          ...todo, complete: !todo.complete
+        }
+        : todo
+    }))
+  }
+
+
+  useEffect(()=>{
+    const todos = JSON.parse(localStorage.getItem(LS_KEY)||[]) 
+    console.log("Test",todos)
+    setTodos(todos)
+  }, [])
+
+  useEffect(()=>{
+    localStorage.setItem(LS_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  return (
+    <div className='todo-list'>
+      <Control addTodo={addTodo} />
+      <Todos removeToDo={removeToDo} toggleTodo={toggleTodo} todos={todos} />
+    </div>
+  )
+}
+
+export default TodoList
